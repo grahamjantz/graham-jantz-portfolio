24f212493b514f166205d185ad0f0957cff3a7ba
grahamjantz <grahammrjantz@gmail.com>
2023-04-12 13:18:50 -0600

    edited bio and minor font tweaks

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -27,16 +27,16 @@ const About = () => {
         <h2>About Me</h2>
         <img src={heroDrums} alt='drum hero' style={imgStyle}/>
         <div className="about-bio">
-          <p className="bio">I am a Front End Developer specializing in React, Redux, and JavaScript. Using these skills, I have created several projects such as: a Monopoly money tracking web app, the official website for the band Waterfront, a PokeDex using the PokeAPI, a static wedding website acting as a landing page for guests to get info, view the registry, and rsvp, as well as several smaller projects intended as practice opportunities for learning new skills. 
+          <p className="bio">I am a self taught Front End Developer specializing in React, Redux, and JavaScript. Using these skills, I have created several projects such as: a Monopoly money tracking web app, the official website for the band Waterfront, a PokeDex using the PokeAPI, a static wedding website acting as a landing page for guests to get info, view the registry, and rsvp, as well as several smaller projects intended as practice opportunities for learning new skills. 
           {/*I am a trained musician looking to change careers and become a Front-End Web Developer! I have a degree in music from the University of Lethbridge and Diploma in Percussion Studies from Red Deer College.*/}</p> 
           <p className="bio">
-          In addition to my main focuses, I have explored additional topics including: Firebase/Firestore, Node.js, SQL, PostgreSQL, Express.js, C, Vue.js, Phaser.js, and Cyber Security to name a few. 
+          In addition to my main focuses, I have explored additional topics including: Python, Express.js, Firebase/Firestore, Node.js, SQL, PostgreSQL, C, Vue.js, Phaser.js, and Cyber Security to name a few. 
           </p>
           <p className="bio">My previous professional experience has given me invaluable administration skills such as: scheduling, communication, and customer service. 
             
           {/* I began my developer studies in May 2022 and have been loving it. I enjoy a good puzzle and solving problems, so I thoroughly enjoy the challenge of web development.*/}
           </p> 
-          <p className="bio">I possess a B.A. degree in music from the University of Lethbridge and a Music Diploma in percussion from Red Deer Polytechnic. In these programs I learned valuable skills such as: time management, project coordination, and collaborating with a team.
+          <p className="bio">I possess a B.A. degree in from the University of Lethbridge and a Diploma from Red Deer Polytechnic. In these programs I learned valuable skills such as: time management, project coordination, and collaborating with a team.
 
           {/* Currently I am working through Codecademy's Front End Development Career Path. But I have also completed freeCodeCamps Responsive Web Design Certificate. */}
           </p>
