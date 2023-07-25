cbd207a2d4ab7a198c29d1440feccfbdb6c73bf7
grahamjantz <grahammrjantz@gmail.com>
2023-07-25 15:39:43 -0600

    added film-catalog to portfolio, commented out inspirational homepage and reddit minimal

--- src/features/Portfolio/Portfolio.js
+++ src/features/Portfolio/Portfolio.js
@@ -10,10 +10,11 @@ import { FaExternalLinkAlt } from 'react-icons/fa'
 import grahamAndMaddyV2 from '../../images/grahamandmaddyV3.jpeg'
 import waterfrontOfficialV2 from '../../images/waterfrontofficialV3.png'
 import pokedexLaptop from '../../images/pokedex.png'
-import inspirationalHomepage from '../../images/homepage.png'
-import redditMinimal from '../../images/redditMinimal.png'
+// import inspirationalHomepage from '../../images/homepage.png'
+// import redditMinimal from '../../images/redditMinimal.png'
 import monopolymoney from '../../images/monopolymoney.png'
 import catan from '../../images/catan.png'
+import film from '../../images/film-catalog.png'
 
 const Portfolio = () => {
 
@@ -38,6 +39,12 @@ const Portfolio = () => {
   //   };
 
     const projects = [
+      {
+          title: "Film Catalog",
+          description: 'This platform is a place for users to keep notes of photos they\'ve taken on film and record the camera settings used for each shot. I was looking for this type of solution and could not find one so I built one myself! Firebase is used for the authentication and database. Users can track multiple cameras with multiple rolls of film.',
+          src: film,
+          liveLink: "https://film.grahamjantz.com/",
+      },
       {
           title: "Monopoly Money",
           description: 'This web app was built as a solution to a problem I had: unorganized Monopoly money! This app uses React and a firestore database to allow players to disregard the paper Monopoly money and keep track of bank accounts, property value, and net worth all within the app. Players can either host or join a room and control their money from their own device. Players will get real time updates from the database in order to collect or spend money and keep track of who is winning!',
@@ -73,20 +80,20 @@ const Portfolio = () => {
           liveLink: "https://pokedex.grahamjantz.com",
           repoLink: "https://github.com/grahamjantz/pokedex",
       },
-      {
-          title: "Inspirational Homepage",
-          description: 'Created as a final project in a Codecademy course, this app is built with React and Redux. The global state managed by Redux contains four main slices, three of which use asynchronous thunks to fetch data from the following API\'s: OpenWeather API, Pexels API, and QuoteOfTheDay API. This project allowed me to gain an in depth knowledge of Redux, using slices, and dispatching actions.',
-          src: inspirationalHomepage,
-          liveLink: "https://inspirational-homepage.grahamjantz.com/",
-          repoLink: "https://github.com/grahamjantz/inspirational-homepage",
-      },
-      {
-          title: "Reddit Minimal",
-          description: 'Reddit Minimal was built as the capstone project in the Front End Development unit of Codecademy\'s Full Stack Development Career Path. The site is built in React and uses Redux to manage the state of posts fetched using the Reddit JSON API. Users can select and view posts from certain subreddits, search for posts, view comments, and are shown a modal view of posts when clicking on the title of each post.',
-          src: redditMinimal,
-          liveLink: "https://reddit.grahamjantz.com/",
-          repoLink: "https://github.com/grahamjantz/reddit-minimal",
-      },
+      // {
+      //     title: "Inspirational Homepage",
+      //     description: 'Created as a final project in a Codecademy course, this app is built with React and Redux. The global state managed by Redux contains four main slices, three of which use asynchronous thunks to fetch data from the following API\'s: OpenWeather API, Pexels API, and QuoteOfTheDay API. This project allowed me to gain an in depth knowledge of Redux, using slices, and dispatching actions.',
+      //     src: inspirationalHomepage,
+      //     liveLink: "https://inspirational-homepage.grahamjantz.com/",
+      //     repoLink: "https://github.com/grahamjantz/inspirational-homepage",
+      // },
+      // {
+      //     title: "Reddit Minimal",
+      //     description: 'Reddit Minimal was built as the capstone project in the Front End Development unit of Codecademy\'s Full Stack Development Career Path. The site is built in React and uses Redux to manage the state of posts fetched using the Reddit JSON API. Users can select and view posts from certain subreddits, search for posts, view comments, and are shown a modal view of posts when clicking on the title of each post.',
+      //     src: redditMinimal,
+      //     liveLink: "https://reddit.grahamjantz.com/",
+      //     repoLink: "https://github.com/grahamjantz/reddit-minimal",
+      // },
     ]
 
     const projectStyle = {
@@ -127,22 +134,36 @@ const Portfolio = () => {
         }}
       />
       <div className='projects'>
-        {projects.map((project) => {
-          return (
-              <div className='project' style={projectStyle} key={project.title}>
-                  <h2>{project.title}</h2>
-                  <img src={project.src} alt='project'/>
-                  <div className='project-buttons'>
+      <div className='project' style={projectStyle} key={projects[0].title}>
+                  <h2>{projects[0].title}</h2>
+                  <img src={projects[0].src} alt='project' style={{width: 'auto', maxHeight: '300px'}}/>
+                  <div className='project-buttons' style={{justifyContent: 'center'}}>
                       <button>
-                        <a href={project.liveLink} rel="noreferrer" target='_blank' style={aStyle}>Live Link<FaExternalLinkAlt /></a>
-                      </button>
-                      <button>
-                        <a href={project.repoLink} rel="noreferrer" target='_blank' style={aStyle}>Repo Link<FaExternalLinkAlt /></a>
+                        <a href={projects[0].liveLink} rel="noreferrer" target='_blank' style={aStyle}>Live Link<FaExternalLinkAlt /></a>
                       </button>
                   </div>
-                  <p>{project.description}</p>
+                  <p>{projects[0].description}</p>
               </div>
-          )
+        {projects.map((project) => {
+          if (project.title !== 'Film Catalog') {
+            return (
+                <div className='project' style={projectStyle} key={project.title}>
+                    <h2>{project.title}</h2>
+                    <img src={project.src} alt='project'/>
+                    <div className='project-buttons'>
+                        <button>
+                          <a href={project.liveLink} rel="noreferrer" target='_blank' style={aStyle}>Live Link<FaExternalLinkAlt /></a>
+                        </button>
+                        <button>
+                          <a href={project.repoLink} rel="noreferrer" target='_blank' style={aStyle}>Repo Link<FaExternalLinkAlt /></a>
+                        </button>
+                    </div>
+                    <p>{project.description}</p>
+                </div>
+            )
+          } else {
+            return ''
+          }
         })}
       </div>
     </div>
